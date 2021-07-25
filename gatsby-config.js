e931bf0431e24ef8200e86b8b62f519b0182fa89
Ivan Smirnov <xsmirx@gmail.com>
2021-07-25 19:29:18 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "xsmirx.com",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-99GX4ZJYXZ",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
