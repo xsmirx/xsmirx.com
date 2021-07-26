59749167869441924bc65b01d10c4d819765e868
Ivan Smirnov <xsmirx@gmail.com>
2021-07-27 00:35:17 +0300

    remove color from typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -13,8 +13,8 @@ const typography = new Typography({
   ],
   headerFontFamily: ["Merriweather", "Georgia", "serif"],
   bodyFontFamily: ["Merriweather", "Georgia", "serif"],
-  headerColor: "hsla(0,0%,0%,1)",
-  bodyColor: "hsla(0,0%,0%,0.9)",
+  // headerColor: "hsla(0,0%,0%,1)",
+  // bodyColor: "hsla(0,0%,0%,0.9)",
   headerWeight: 900,
   bodyWeight: 400,
   boldWeight: 700,
@@ -23,7 +23,7 @@ const typography = new Typography({
   overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
     blockquote: {
       ...scale(1 / 5),
-      color: "hsla(0,0%,100%,0.7)",
+      // color: "hsla(0,0%,100%,0.7)",
       fontStyle: "italic",
       paddingLeft: rhythm(13 / 16),
       marginLeft: rhythm(-1),
@@ -56,24 +56,13 @@ const typography = new Typography({
         paddingLeft: rhythm(9 / 16),
       },
     },
-    h4: {
-      letterSpacing: "0.140625em",
-      textTransform: "uppercase",
-    },
-    h6: {
-      fontStyle: "italic",
-    },
     a: {
-      boxShadow: "0 1px 0 0 currentColor",
-      color: "#007acc",
       textDecoration: "none",
     },
     "a:hover,a:active": {
       boxShadow: "none",
     },
     "mark,ins": {
-      background: "#007acc",
-      color: "white",
       padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
       textDecoration: "none",
     },
