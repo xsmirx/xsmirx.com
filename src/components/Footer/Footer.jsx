3beec78b35ab39f99bf36b4d658b36ea477c3968
Ivan Smirnov <xsmirx@gmail.com>
2021-07-27 02:23:29 +0300

    Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
-const Footer = () => {
-  return <p>footer</p>
+const StyledFooter = styled.footer`
+  margin: 0 auto;
+  padding: 0 3%;
+  max-width: 80%;
+  height: 55px;
+  border-top: 2px solid ${(props) => props.theme.linkColor};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  p {
+    font-size: 0.8rem;
+  }
+`;
+
+const Footer = (props) => {
+  const currenFullYeaer = new Date().getFullYear();
+  return (
+    <StyledFooter>
+      <h6>© 2020 - {currenFullYeaer}</h6>
+      <h6>Built by Ivan Smirnov</h6>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
