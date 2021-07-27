c8dee6dcf2ed90e37b9480946c5f6d45e3ff98c2
Ivan Smirnov <xsmirx@gmail.com>
2021-07-27 03:05:44 +0300

    added new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import * as React from "react";
 import AboutMe from "../components/AboutMe/AboutMe";
+import Contacts from "../components/Contacts/Contacts";
 import Layout from "../components/Layout";
+import MyPropjects from "../components/MyProjects/MyProjects";
+import Skills from "../components/Skills/Skills";
 import Title from "../components/Title/Title";
 
 const IndexPage = (props) => {
@@ -8,13 +11,15 @@ const IndexPage = (props) => {
     <Layout pageTitle="Ivan Smirnov" {...props}>
       <Title />
       <AboutMe />
+      <Skills />
+      <MyPropjects />
+      <Contacts />
       {
         // About me (photo, text, social contacts and email)
         // skills (soft, stack, plans)
         // My projectsfrom GitHub
         // contacts (btn mailto)
       }
-      <p>Content</p>
     </Layout>
   );
 };
