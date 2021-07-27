eb09a1c8ae86ca63222ce6c58f29ecf558b09b21
Ivan Smirnov <xsmirx@gmail.com>
2021-07-27 21:24:44 +0300

    added StyledSection

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import StyledSectionTitle from "../../styles/StyledSectionTitle";
+import StyledSection from "./../../styles/StyledSection";
 
 const MyPropjects = () => {
   return (
-    <div>
-      <h2>My projects</h2>
-    </div>
+    <StyledSection>
+      <StyledSectionTitle>
+        <h2>My projects</h2>
+      </StyledSectionTitle>
+    </StyledSection>
   );
 };
 
