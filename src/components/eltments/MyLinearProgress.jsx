f55c8db0091f8426a3a2a1a96485df9d3fd06718
Ivan Smirnov <xsmirx@gmail.com>
2021-07-28 03:10:01 +0300

    MyLinearProgress component

---
+++ src/components/eltments/MyLinearProgress.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { LinearProgress, withStyles } from "@material-ui/core";
+import styled from "styled-components";
+
+const StyledBlock = styled.div`
+  p {
+    font-size: 1rem;
+  }
+`;
+
+const StyledLinearPropgress = withStyles({
+  // shit! I need use ThemeProvider for this background-color!
+  barColorPrimary: { "background-color": "#00adb5" },
+})(LinearProgress);
+
+const MyLinearProgress = ({ skill, value }) => {
+  return (
+    <StyledBlock>
+      <p>{skill}</p>
+      <StyledLinearPropgress variant="buffer" value={value} />
+    </StyledBlock>
+  );
+};
+
+export default MyLinearProgress;
