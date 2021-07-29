1895fd493f8efa6b905c9ac634fc0ae062a8b537
Ivan Smirnov <xsmirx@gmail.com>
2021-07-29 17:30:23 +0300

    added plubin mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,23 +9,26 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: { extensions: [`.mdx`, `.md`] },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: "./src/content",
+      },
+    },
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "G-99GX4ZJYXZ",
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    // {
-    //   resolve: "gatsby-source-filesystem",
-    //   options: {
-    //     name: "images",
-    //     path: "./src/images/",
-    //   },
-    //   __key: "images",
-    // },
     {
       resolve: "gatsby-plugin-typography",
       options: {
